322efdee38608d62206e2b05cc0a93c861b2614b
Szymon <sz@ympek.net>
2021-08-26 09:36:06 +0200

    Make release a draft. Add tag. Fix wrong repo for docker. (#11)

--- core/husarnet_config.h
+++ core/husarnet_config.h
@@ -3,7 +3,7 @@
 #include "ipaddress.h"
 #include <vector>
 
-#define HUSARNET_VERSION "2021.08.25.2"
+#define HUSARNET_VERSION "2021.08.26.1"
 
 __attribute__((unused)) static const InetAddress baseTcpAddresses[] = {
     {IpAddress::parse("188.165.23.196"), 443}, // ovh RBX (base-2)
