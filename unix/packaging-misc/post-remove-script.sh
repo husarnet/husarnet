721529885b2e916432ed7221c2c5ffaf53332f38
KonradPr <konrad.przewloka@gmail.com>
2021-10-27 14:46:41 +0200

    post review update

--- unix/packaging-misc/post-remove-script.sh
+++ unix/packaging-misc/post-remove-script.sh
@@ -5,7 +5,7 @@ input="/var/lib/husarnet/ip6tables_rules"
 if test -f "$input"; then
   while IFS= read  line
   do
-    while ip6tables --delete $line 2> /dev/null; do :; done
+    while ip6tables --delete "$line" 2> /dev/null; do :; done
 
   done < "$input"
 fi
