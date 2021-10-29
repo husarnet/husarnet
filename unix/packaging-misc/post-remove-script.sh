534a1f409052d783d6ec0c966a44ef9a5e0cfb8d
KonradPR <32310362+KonradPR@users.noreply.github.com>
2021-10-29 15:09:51 +0200

    fix for known errors in post-remove (#20)
    * fix for known errors in post-remove

--- unix/packaging-misc/post-remove-script.sh
+++ unix/packaging-misc/post-remove-script.sh
@@ -2,9 +2,11 @@
 set -euo pipefail
 
 input="/var/lib/husarnet/ip6tables_rules"
-while IFS= read  line
-do
-  ip6tables --delete $line
-done < "$input"
+if test -f "$input"; then
+  while IFS= read  line
+  do
+    while ip6tables --delete "$line" 2> /dev/null; do :; done
 
+  done < "$input"
+fi
 rm -fr /var/lib/husarnet
