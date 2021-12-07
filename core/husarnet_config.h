3822a64b8823c9255c7b9bb3cfd897e47d3cf863
Husarnet CI man <szymon.niemiec@husarnet.com>
2021-12-07 12:44:22 +0000

    Automatic version bump. The PR branch is ready to be merged.

--- core/husarnet_config.h
+++ core/husarnet_config.h
@@ -3,7 +3,7 @@
 #include "ipaddress.h"
 #include <vector>
 
-#define HUSARNET_VERSION "2021.11.26.1"
+#define HUSARNET_VERSION "2021.12.07.1"
 
 __attribute__((unused)) static const InetAddress baseTcpAddresses[] = {
     {IpAddress::parse("188.165.23.196"), 443}, // ovh RBX (base-2)
