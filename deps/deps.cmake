0a0f20e68eeb2631d4f9e5a954bae2f9b31e03a6
Paweł pidpawel Kozubal <git@pidpawel.eu>
2022-02-07 20:16:39 +0100

    Remove some orphaned submodules

--- deps/deps.cmake
+++ deps/deps.cmake
@@ -1,5 +1,2 @@
 include(${CMAKE_CURRENT_LIST_DIR}/sqlite3.cmake)
-include(${CMAKE_CURRENT_LIST_DIR}/zstd.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/sodium.cmake)
-
-include_directories(${D}/ArduinoJson)
