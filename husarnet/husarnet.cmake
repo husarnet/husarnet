0cf422658e6ad1dab9c7db4b4ca1fca54fa588f5
Paweł pidpawel Kozubal <git@pidpawel.eu>
2022-02-11 00:39:29 +0100

    Fix cmake

--- husarnet/husarnet.cmake
+++ husarnet/husarnet.cmake
@@ -93,7 +93,7 @@ add_library(husarnet_core STATIC ${husarnet_core_SRC})
 include(FetchContent)
 
 include(${CMAKE_CURRENT_LIST_DIR}/../deps/deps.cmake)
-target_link_libraries(husarnet_core sodium sqlite3)
+target_link_libraries(husarnet_core sodium)
 
 if (${CMAKE_SYSTEM_NAME} STREQUAL Windows)
   target_link_libraries(husarnet_core iphlpapi shlwapi ws2_32)
