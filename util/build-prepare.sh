f68e1e22ca26908295ae1fe44d7e3879b44bf1d3
Paweł Kozubal <pidpawel@users.noreply.github.com>
2022-06-28 15:43:06 +0200

    Add python3 to dependencies (#46)

--- util/build-prepare.sh
+++ util/build-prepare.sh
@@ -9,7 +9,7 @@ sudo apt install -y git
 git submodule update --init --recursive
 
 # Install stuff for cross building
-sudo apt install -y ninja-build cmake linux-headers-generic build-essential crossbuild-essential-i386 crossbuild-essential-amd64 crossbuild-essential-arm64 crossbuild-essential-armhf crossbuild-essential-riscv64 g++-mingw-w64
+sudo apt install -y ninja-build cmake python3 linux-headers-generic build-essential crossbuild-essential-i386 crossbuild-essential-amd64 crossbuild-essential-arm64 crossbuild-essential-armhf crossbuild-essential-riscv64 g++-mingw-w64
 
 # This is unfortunate but... (see https://stackoverflow.com/a/38751292 )
 sudo ln -sf /usr/include/asm-generic /usr/include/asm
