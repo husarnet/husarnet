327107a62a77f043a7155114bbdae3f21e5a7c7c
Paweł pidpawel Kozubal <git@pidpawel.eu>
2022-06-29 16:06:28 +0200

    Add HUSARNET_JOIN_CODE environment variable support

--- unix/docker-misc/husarnet-docker.sh
+++ unix/docker-misc/husarnet-docker.sh
@@ -8,58 +8,20 @@ catch() {
 }
 trap 'catch $? $LINENO' EXIT
 
-# If ${JOINCODE} is empty - quit
-if [ -z "${JOINCODE:-}" ]; then
-    echo "You have to set JOINCODE environment variable in order for this container to work"
+# Rewrite old name to new one
+if [ ! -z "${JOINCODE:-}" ]; then
+    HUSARNET_JOIN_CODE="${JOINCODE}"
+fi
+
+# Check whether join code is set
+if [ -z "${HUSARNET_JOIN_CODE:-}" ]; then
+    echo "You have to set HUSARNET_JOIN_CODE environment variable in order for this container to work"
     exit 1
 fi
 
 # Start daemon
 if [ -z "${HUSARNET_DEBUG:-}" ]; then
-    husarnet daemon >/dev/null 2>&1 &
+    HUSARNET_JOIN_CODE="${HUSARNET_JOIN_CODE}" husarnet daemon >/dev/null 2>&1
 else
-    husarnet daemon &
+    HUSARNET_JOIN_CODE="${HUSARNET_JOIN_CODE}" husarnet daemon
 fi
-
-daemon_pid=$!
-
-# Waiting until Husarnet daemon is ready
-echo "[step 1/3] Waiting for Husarnet daemon to start"
-
-while true; do
-    if [ $(husarnet status 2>&1 | grep "request failed" | wc -l) -eq 0 ]; then
-        break
-    elif [ ! -e /proc/${daemon_pid} ]; then
-        echo "Daemon has quit unexpectedly!"
-        exit 2
-    else
-        echo "..."
-        sleep 1
-    fi
-done
-
-echo "done"
-echo ""
-
-# Waiting until Base Server connection is established
-echo "[step 2/3] Waiting for Base Server connection"
-
-while [ $(husarnet status | grep "ERROR" | wc -l) -gt 0 ]; 
-do 
-    echo "..."
-    sleep 1
-done
-
-echo "done"
-echo ""
-
-# Joining the network
-echo "[step 3/3] Joining to Husarnet network"
-
-husarnet join ${JOINCODE} ${HOSTNAME}
-
-# Print status
-husarnet status | grep "Husarnet IP address"
-
-# Keep this process running until daemon quits for any reason
-wait ${daemon_pid}
