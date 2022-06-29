c64ee4cd7e3c79d60f50148affbbb6d9c9d23009
Paweł Kozubal <pidpawel@users.noreply.github.com>
2022-06-29 14:10:08 +0200

    Update clang-format to 14 (#50)

--- util/format.sh
+++ util/format.sh
@@ -2,5 +2,5 @@
 source $(dirname "$0")/bash-base.sh
 
 pushd ${base_dir}
-find . -regextype posix-egrep -regex ".*\.(cpp|hpp|h|c)" -not -path "./build/*" -exec clang-format-12 -style=file -i {} \;
+find . -regextype posix-egrep -regex ".*\.(cpp|hpp|h|c)" -not -path "./build/*" -exec clang-format-14 -style=file -i {} \;
 popd
