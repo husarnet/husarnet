11ee9b9480357215bf1e270bc72bd919a7dbbe58
Paweł Kozubal <pidpawel@users.noreply.github.com>
2022-08-17 18:43:30 +0200

    Remove CLI's todo (#76)

--- cli/TODO.md
+++
@@ -1,5 +0,0 @@
-# TODO
-
-- allow for removal/update via name, not an ID
-- GET transport method for queries (defaults to POST)
-- some user friendliness, validating input e.g. we expected number here
--- cli/messages.go
+++ cli/messages.go
@@ -3,7 +3,7 @@
 // License: specified in project_root/LICENSE.txt
 package main
 
-const version string = "2022.08.17.2"
+const version string = "2022.08.17.4"
 
 const credentialsPrompt string = "Please provide your credentials to Husarnet Dashboard."
 const removeGroupConfirmationPrompt string = "Are you sure you want to delete this group? All the devices inside the group will be deleted as well."
