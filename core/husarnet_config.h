2ce0ecb0443ac29720204400a20c9905313f7028
Husarnet CI man <szymon.niemiec@husarnet.com>
2022-09-02 12:50:54 +0000

    Automatic version bump

--- core/husarnet_config.h
+++ core/husarnet_config.h
@@ -8,7 +8,7 @@
 #include "husarnet/config_storage.h"
 #include "husarnet/ipaddress.h"
 
-#define HUSARNET_VERSION "2022.09.02.10"
+#define HUSARNET_VERSION "2022.09.02.11"
 #define WEBSETUP_SERVER_PORT 5580
 #define WEBSETUP_CLIENT_PORT 4800
 #define BASESERVER_PORT 443
