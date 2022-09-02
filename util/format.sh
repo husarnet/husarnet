eb2ffeb2558d1b2eb82f99a9fc71a45ff4e6a054
Paweł pidpawel Kozubal <git@pidpawel.eu>
2022-09-02 16:21:13 +0200

    Move formatting check to script

--- util/format.sh
+++ util/format.sh
@@ -3,4 +3,14 @@ source $(dirname "$0")/bash-base.sh
 
 pushd ${base_dir}
 find . -regextype posix-egrep -regex ".*\.(cpp|hpp|h|c)" -not -path "./build/*" -exec clang-format-14 -style=file -i {} \;
+
+git diff # to print changes to logs
+
 popd
+
+if [ $(git diff | wc -l) -gt 0 ]; then
+    echo Found differences!
+    exit 1
+else
+    exit 0
+fi
