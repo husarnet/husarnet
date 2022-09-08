5e78e869999b1705a869284f63951772b6d8b76e
ympek <sz@ympek.net>
2022-09-08 12:05:42 +0200

    proposed changes to readme; remove unused deploy-prepare.sh

--- util/deploy-prepare.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-source $(dirname "$0")/bash-base.sh
-
--- util/prepare-all.sh
+++ util/prepare-all.sh
@@ -3,4 +3,3 @@ source $(dirname "$0")/bash-base.sh
 
 ${util_base}/build-prepare.sh
 ${util_base}/test-prepare.sh
-${util_base}/deploy-prepare.sh
