e3475e00057b7e8292c074e0726fc54d11af753d
pidpawel <pidpawel@users.noreply.github.com>
2022-10-18 18:04:54 +0000

    Automatic version bump

--- core/husarnet_config.h
+++ core/husarnet_config.h
@@ -8,7 +8,7 @@
 #include "husarnet/config_storage.h"
 #include "husarnet/ipaddress.h"
 
-#define HUSARNET_VERSION "2022.10.18.2"
+#define HUSARNET_VERSION "2022.10.18.3"
 #define WEBSETUP_SERVER_PORT 5580
 #define WEBSETUP_CLIENT_PORT 4800
 #define BASESERVER_PORT 443
