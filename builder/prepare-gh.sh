c7f78446f2eb08b3ab08d0ca5717c782aa16ba35
Paweł pidpawel Kozubal <git@pidpawel.eu>
2022-10-18 20:14:10 +0200

    Fix docker compose exit codes and file permissions

---
+++ builder/prepare-gh.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+source $(dirname "$0")/../util/bash-base.sh
+
+mkdir -p ${build_base}
+chown 1000:1000 ${build_base}
+chmod 777 ${build_base}
--- util/test.sh
+++ util/test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source $(dirname "$0")/bash-base.sh
 
-docker compose -f builder/compose.yml up test
+docker compose -f builder/compose.yml up --exit-code-from test test
