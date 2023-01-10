123da432a3dabd8816ff8e2d77c00c0f2f06d54d
fwlodarczyk <wlodarczyk.filip@int.pl>
2023-01-10 11:36:46 +0100

    fixes to integration test

--- tests/integration/integration-test.sh
+++ tests/integration/integration-test.sh
@@ -3,12 +3,14 @@
 #Script is intended to be run from Superuser account.
 #Usage: ./integration-test.sh [JOIN_CODE] [Husarnet Dashboard User Name] [Husarnet Dashboard Password] [Husarnet Network Name]
 curl -s https://install.husarnet.com/install.sh | sudo bash
-CLEANUP=$(husarnet dashboard login ${2} ${3} && husarnet dashboard rm $(cat /etc/hostname) ${4})
+function CLEANUP {
+husarnet dashboard login "${2}" "${3}" && husarnet dashboard rm $(cat /etc/hostname) "${4}"
+}
 husarnet-daemon &
 husarnet daemon wait daemon
 husarnet daemon wait base
 husarnet join ${1} $(cat /etc/hostname)
-husarnet status | grep -c 'Is joined?                 yes'
+husarnet status | grep 'Is joined?' | grep 'yes'
 if [ ${?} -ne 0 ]; then
    echo 'Husarnet failed to join network'
    exit 1
@@ -18,11 +20,11 @@ fi
 cat /etc/hosts | grep -c '# managed by Husarnet'
 if [ ${?} -ne 0 ]; then
    echo 'Husarnet failed saving hostnames to /etc/hosts file, cleaning up and exiting...'
-   ${CLEANUP}
+   CLEANUP
    exit 1
 else
    echo '/etc/hosts file is ok'
 fi
 echo 'Basic integrity test went ok, cleaning up, and exitting...'
-${CLEANUP}
+CLEANUP
 exit 0
