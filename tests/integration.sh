cfc572ab340405939e09f4eac1fc420942f54ea1
Paweł pidpawel Kozubal <git@pidpawel.eu>
2023-01-11 13:20:27 +0100

    Add integration tests for Debian, add procps to deb requirements, remove old Fedora integration tests

--- tests/integration.sh
+++ tests/integration.sh
@@ -9,9 +9,10 @@ docker compose -f builder/compose.yml up --exit-code-from test_docker test_docke
 docker compose -f builder/compose.yml up --exit-code-from test_ubuntu_18_04 test_ubuntu_18_04
 docker compose -f builder/compose.yml up --exit-code-from test_ubuntu_20_04 test_ubuntu_20_04
 docker compose -f builder/compose.yml up --exit-code-from test_ubuntu_22_04 test_ubuntu_22_04
-# docker compose -f builder/compose.yml up --exit-code-from test_fedora_35 test_fedora_35
-# docker compose -f builder/compose.yml up --exit-code-from test_fedora_36 test_fedora_36
-# docker compose -f builder/compose.yml up --exit-code-from test_fedora_37 test_fedora_37
+docker compose -f builder/compose.yml up --exit-code-from test_debian_oldstable test_debian_oldstable
+docker compose -f builder/compose.yml up --exit-code-from test_debian_stable test_debian_stable
+docker compose -f builder/compose.yml up --exit-code-from test_debian_testing test_debian_testing
+docker compose -f builder/compose.yml up --exit-code-from test_fedora_37 test_fedora_37
 docker compose -f builder/compose.yml up --exit-code-from test_fedora_38 test_fedora_38
 
 popd
--- tests/integration/all-tests.sh
+++ tests/integration/all-tests.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 source $(dirname "$0")/../../util/bash-base.sh
 
+# WARNING this script is intended to be ran from inside Docker containers ONLY
+
 echo "--------------------------------------------"
 
 ${tests_base}/integration/tests/functional-basic.sh
