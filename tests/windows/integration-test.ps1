8579338fde95eb1bb171db7830cb089e383ad713
fwlodarczyk <wlodarczyk.filip@int.pl>
2023-02-28 09:21:56 +0100

    powershell int test should work

--- tests/windows/integration-test.ps1
+++ tests/windows/integration-test.ps1
@@ -2,3 +2,11 @@ Copy-Item ..\..\build\release\husarnet-daemon-windows-win64.exe -Destination C:\
 Copy-Item ..\..\build\release\husarnet-windows-win64.exe -Destination C:\Program Files\Husarnet\husarnet.exe -Force
 Restart-Service -Name husarnet -Force
 husarnet join $joincode
+husarnet status | Select-String 'Is joined?                 yes' -Quiet -SimpleMatch
+if ($LASTEXITCODE -eq 0) {
+    Write-Host "OK."
+    exit 0
+} else {
+    Write-Host "Nie ok."
+    exit 1
+}
