d49bda197f668814144b4769fe152bce391f69e2
ympek <sz@ympek.net>
2023-03-13 18:17:10 +0100

    cli: fix copyright

--- cli/service_commands.go
+++ cli/service_commands.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2023 Husarnet sp. z o.o.
+// Copyright (c) 2022 Husarnet sp. z o.o.
 // Authors: listed in project_root/README.md
 // License: specified in project_root/LICENSE.txt
 package main
