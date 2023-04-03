0432b0a3b68797131de5925c83a0362500dab7b1
Paweł pidpawel Kozubal <git@pidpawel.eu>
2023-04-03 14:28:50 +0200

    util: Fix integration tests workdir permissions

--- builder/entrypoint.sh
+++ builder/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source $(dirname "$0")/../util/bash-base.sh
 
-fix_perms () {
+fix_perms() {
     mkdir -p $1
     chown -R 1000:1000 $1
     chmod 777 $1
@@ -9,5 +9,6 @@ fix_perms () {
 
 fix_perms ${build_base}
 fix_perms ${base_dir}/cli/generated
+fix_perms ${base_dir}/tests/integration
 
 su -w "CGO_ENABLED,GOCACHE,GOFLAGS,IDF_PATH,IDF_TOOLS_PATH,IDF_PYTHON_CHECK_CONSTRAINTS" -l -P builder -c "/usr/bin/bash ${*}"
