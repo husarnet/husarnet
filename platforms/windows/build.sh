eb627293664ac0fb74d9fda32db8a24c02842ccd
fwlodarczyk <wlodarczyk.filip@int.pl>
2023-05-30 12:47:00 +0200

    kinda works

--- platforms/windows/build.sh
+++ platforms/windows/build.sh
@@ -3,8 +3,8 @@ source $(dirname "$0")/../../util/bash-base.sh
 
 platform=windows
 arch=win64
-
+build_type=stable
 platform_base=${base_dir}/platforms/${platform}
 
-${base_dir}/daemon/build.sh ${platform} ${arch}
+${base_dir}/daemon/build.sh ${platform} ${arch} ${build_type}
 ${base_dir}/cli/build.sh ${platform} ${arch}
