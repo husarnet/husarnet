191f68d8543f5c230bd8de6408bc2b482a90c415
Paweł pidpawel Kozubal <git@pidpawel.eu>
2023-05-30 22:35:37 +0200

    core: Try building daemon with predefined instruction set, even on amd64

--- daemon/arch_amd64.cmake
+++ daemon/arch_amd64.cmake
@@ -2,9 +2,10 @@ set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR AMD64)
 
 set(triple x86_64-linux-musl)
+set(zig_extra -march=x86_64_v3 -mtune=x86_64_v3)
 
-set(CMAKE_C_COMPILER "zig" cc -target ${triple})
-set(CMAKE_CXX_COMPILER "zig" c++ -target ${triple})
+set(CMAKE_C_COMPILER "zig" cc -target ${triple} ${zig_extra})
+set(CMAKE_CXX_COMPILER "zig" c++ -target ${triple} ${zig_extra})
 
 set(CMAKE_AR "${CMAKE_CURRENT_LIST_DIR}/zig-ar.sh")
 set(CMAKE_RANLIB "${CMAKE_CURRENT_LIST_DIR}/zig-ranlib.sh")
