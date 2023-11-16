116a556e028922d3ae289a0d313d221686d20755
fwlodarczyk <wlodarczyk.filip@int.pl>
2023-11-16 08:28:34 +0100

    core: update copyrights

--- core/libsodium-config/sodium/version.h
+++ core/libsodium-config/sodium/version.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2022 Husarnet sp. z o.o.
+// Copyright (c) 2024 Husarnet sp. z o.o.
 // Authors: listed in project_root/README.md
 // License: specified in project_root/LICENSE.txt
 #ifndef sodium_version_H
--- daemon/zig-ranlib.sh
+++ daemon/zig-ranlib.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (c) 2022 Husarnet sp. z o.o.
+# Copyright (c) 2024 Husarnet sp. z o.o.
 # Authors: listed in project_root/README.md
 # License: specified in project_root/LICENSE.txt
 zig ranlib "$@"
