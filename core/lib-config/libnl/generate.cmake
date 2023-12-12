bb4664c3301b982bfcbbf97c18fa6cdc2d71287c
Miłosz Łagan <milosz.lagan@husarion.com>
2023-12-12 17:35:42 +0100

    Minor stylistic changes

--- core/lib-config/libnl/generate.cmake
+++ core/lib-config/libnl/generate.cmake
@@ -16,8 +16,8 @@ set(LT_AGE ${LIBNL_AGE})
 
 configure_file(${libnl_SOURCE_DIR}/include/netlink/version.h.in ${libnl_SOURCE_DIR}/include/netlink/version.h @ONLY)
 
-
 # Generate config.h file
 # This file orginally used autoconf to generate define feature flags, but cmake automatically populates them for us
 # It is left for compatibility with existing code and time.h include workaround (see config.h.in file)
+
 configure_file(${CMAKE_CURRENT_LIST_DIR}/config.h.in ${libnl_SOURCE_DIR}/include/config.h @ONLY)
