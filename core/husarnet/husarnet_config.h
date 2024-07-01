705bf3fe97151ca35c02355e9e0e6f9c91c7a56d
miloszlagan <miloszlagan@users.noreply.github.com>
2024-07-01 10:19:45 +0000

    Build version 2.0.258

--- core/husarnet/husarnet_config.h
+++ core/husarnet/husarnet_config.h
@@ -17,7 +17,7 @@
 #include "sdkconfig.h"
 #endif
 
-#define HUSARNET_VERSION "2.0.257"
+#define HUSARNET_VERSION "2.0.258"
 #define WEBSETUP_SERVER_PORT 5580
 #define WEBSETUP_CLIENT_PORT 4800
 #define BASESERVER_PORT 443
