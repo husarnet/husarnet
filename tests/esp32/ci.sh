94fee859081ca4dcb19faa5c9c0b6bb3a313f470
Milosz Lagan <milosz.lagan@husarion.com>
2024-07-05 12:13:02 +0200

    tests: cleanup after ESP32 CI

--- tests/esp32/ci.sh
+++ tests/esp32/ci.sh
@@ -6,6 +6,10 @@ source $(dirname "$0")/../../util/bash-base.sh
 
 source $(dirname "$0")/../secrets-decrypted.sh
 
+runner_hostname=$(hostname -f)
+esp32_hostname="esp32-integration-test"
+network_name="integration_tests_esp"
+
 # Initialize ESP-IDF environment
 if ! command -v idf.py &> /dev/null; then
   echo "ESP-IDF environment is not initialized, initializing..."
@@ -24,18 +28,38 @@ if ! command -v husarnet &> /dev/null; then
 
   # Start husarnet daemon
   sudo husarnet-daemon &> /tmp/husarnet-daemon.log &
+  
+  # Join to Husarnet network
+  echo "INFO: Waiting for deamon connectivity"
+  husarnet daemon wait daemon
+  echo "INFO: Waiting for Base Server connectivity"
+  husarnet daemon wait base
   sudo husarnet join ${esp32_join_code} runner-esp32
+else
+  # Those are reduntant but we want to test as many items as possible
+  echo "INFO: Waiting for deamon connectivity"
+  husarnet daemon wait daemon
+  echo "INFO: Waiting for Base Server connectivity"
+  husarnet daemon wait base
 fi
-
-# Those are reduntant but we want to test as many items as possible
-echo "INFO: Waiting for deamon connectivity"
-husarnet daemon wait daemon
-echo "INFO: Waiting for Base Server connectivity"
-husarnet daemon wait base
 
 pushd ${base_dir}/platforms/esp32
 
 # Run tests
 pytest pytest_eth_husarnet.py --target esp32 --junitxml junit.xml --joincode ${esp32_join_code} --app-path ../../build
+
+# Cleanup
+echo "INFO: Cleaning up"
+
+sudo pkill husarnet-daemon
+echo "WARNING: This 'killed' message is a good thing! We're calling it on purpose"
+
+husarnet dashboard login "${dashboard_login}" "${dashboard_pass}"
+husarnet dashboard rm ${runner_hostname} "${network_name}"
+husarnet dashboard rm ${esp32_hostname} "${network_name}"
+husarnet dashboard device rm "${runner_hostname}"
+husarnet dashboard device rm "${esp32_hostname}"
+
+echo "INFO: Cleaned up successfully"
 
 popd
