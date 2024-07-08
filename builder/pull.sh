332633c72cc8d30f38f5071e2fdb0d577d7484dd
Paweł pidpawel Kozubal <git@pidpawel.eu>
2024-07-08 18:50:12 +0200

    builder: Add builder pull script

---
+++ builder/pull.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Copyright (c) 2024 Husarnet sp. z o.o.
+# Authors: listed in project_root/README.md
+# License: specified in project_root/LICENSE.txt
+source $(dirname "$0")/../util/bash-base.sh
+
+docker image pull ghcr.io/husarnet/husarnet:builder
