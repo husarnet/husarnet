de5bcfebbe0e9a2f8ecc0235488326fca92e77b3
fwlodarczyk <wlodarczyk.filip@int.pl>
2024-08-14 13:25:37 +0200

    .

--- platforms/linux/packaging/post-install-script.sh
+++ platforms/linux/packaging/post-install-script.sh
@@ -2,10 +2,10 @@
 # Copyright (c) 2024 Husarnet sp. z o.o.
 # Authors: listed in project_root/README.md
 # License: specified in project_root/LICENSE.txt
-groupadd husarnet 2>&1 >> /dev/null
+groupadd husarnet >> /dev/null 2>&1
 chgrp husarnet /var/lib/husarnet -R
 chmod g+rwx /var/lib/husarnet -R
-sudo usermod -aG husarnet $SUDO_USER 2>&1 >> /dev/null
+sudo usermod -aG husarnet $SUDO_USER >> /dev/null 2>&1
 command -v pidof >/dev/null || exit 0
 command -v systemctl >/dev/null || exit 0
 
