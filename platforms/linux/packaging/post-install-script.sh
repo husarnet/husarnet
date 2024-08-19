4a534d7b714f2d3a532d1b6a9980e4f8e9a97de2
Filip Włodarczyk <116726173+fwlodarczyk@users.noreply.github.com>
2024-08-19 11:54:44 +0200

    Update post-install-script.sh

--- platforms/linux/packaging/post-install-script.sh
+++ platforms/linux/packaging/post-install-script.sh
@@ -2,10 +2,11 @@
 # Copyright (c) 2024 Husarnet sp. z o.o.
 # Authors: listed in project_root/README.md
 # License: specified in project_root/LICENSE.txt
-groupadd husarnet >> /dev/null 2>&1
+if ! grep -q "^husarnet:" /etc/group; then
+    groupadd husarnet
+fi
 chgrp husarnet /var/lib/husarnet -R
 chmod g+rwx /var/lib/husarnet -R
-sudo usermod -aG husarnet $SUDO_USER >> /dev/null 2>&1
 command -v pidof >/dev/null || exit 0
 command -v systemctl >/dev/null || exit 0
 
