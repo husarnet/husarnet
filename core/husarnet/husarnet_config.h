e6a88ac9b8f019d5d0399e0ccace05493c10e8f2
pidpawel <pidpawel@users.noreply.github.com>
2024-10-03 10:53:08 +0000

    Build version 2.0.295

--- core/husarnet/husarnet_config.h
+++ core/husarnet/husarnet_config.h
@@ -17,7 +17,7 @@
 #include "sdkconfig.h"
 #endif
 
-#define HUSARNET_VERSION "2.0.294"
+#define HUSARNET_VERSION "2.0.295"
 #define WEBSETUP_SERVER_PORT 5580
 #define WEBSETUP_CLIENT_PORT 4800
 #define BASESERVER_PORT 443
