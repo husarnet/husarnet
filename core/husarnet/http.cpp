b7de8bfad75a3d6acc79f712df9f649e07986a9c
Milosz Lagan <milosz.lagan@husarion.com>
2024-10-04 16:23:47 +0200

    core: add dynamic http client

--- core/husarnet/http.cpp
+++ core/husarnet/http.cpp
@@ -12,44 +12,104 @@ size_t HTTPMessage::encode(etl::ivector<char>& buffer)
 {
   buffer.clear();
 
-  // Start line
-  etl::array<char, 80> line;
-  size_t len = std::snprintf(
-      line.data(), line.size(), "%s %s HTTP/1.1\r\n",
-      this->request.method.c_str(), this->request.endpoint.c_str());
+  // Start line ({method} {endpoint} HTTP/1.1)
+  buffer.insert(
+      buffer.end(), this->request.method.begin(), this->request.method.end());
+  buffer.push_back(' ');
+  buffer.insert(
+      buffer.end(), this->request.endpoint.begin(),
+      this->request.endpoint.end());
+  etl::string<11> version = " HTTP/1.1\r\n";
+  buffer.insert(buffer.end(), version.begin(), version.end());
 
-  etl::copy(line.begin(), line.begin() + len, etl::back_inserter(buffer));
-
-  // Headers
+  // Headers ({key}: {value})
   for(auto& [key, value] : headers) {
-    len = std::snprintf(
-        line.data(), line.size(), "%s: %s\r\n", key.c_str(), value.c_str());
-    etl::copy(line.begin(), line.begin() + len, etl::back_inserter(buffer));
+    buffer.insert(buffer.end(), key.begin(), key.end());
+    buffer.push_back(':');
+    buffer.push_back(' ');
+    buffer.insert(buffer.end(), value.begin(), value.end());
+    buffer.insert(buffer.end(), EOL_DELIMITER.begin(), EOL_DELIMITER.end());
   }
 
   // Header-body separator
-  buffer.push_back('\r');
-  buffer.push_back('\n');
+  buffer.insert(buffer.end(), BODY_DELIMITER.begin(), BODY_DELIMITER.end());
 
   return buffer.size();
 }
 
-bool HTTPMessage::parse(etl::string_view& buffer_view, HTTPMessage& message)
+HTTPMessage::Result HTTPMessage::_parseStartLine()
 {
-  // TODO: Allow for chunked messages
+  etl::string_view buffer_view(
+      &(*this->_it), std::distance(this->_it, this->_buffer.end()));
 
-  message.headers.clear();
-  message.messageType = Type::UNDEFINED;
+  // Find end of line
+  size_t end = buffer_view.find(EOL_DELIMITER);
+  if(end == etl::string_view::npos)
+    return Result::INCOMPLETE;
+
+  // Split start line by spaces
+  size_t space1 = buffer_view.find(' ');
+  size_t space2 = buffer_view.find(' ', space1 + 1);
+  if(space1 == etl::string_view::npos || space2 == etl::string_view::npos)
+    return Result::INVALID;
+
+  etl::string_view field1 = buffer_view.substr(0, space1);
+  etl::string_view field2 = buffer_view.substr(space1 + 1, space2 - space1 - 1);
+  etl::string_view field3 = buffer_view.substr(space2 + 1, end - space2 - 1);
+
+  if(field1.empty() || field2.empty() || field3.empty())
+    return Result::INVALID;
+
+  // Check if it's a request or response
+  if(field1 == "HTTP/1.1")  // Response
+  {
+    this->messageType = Type::RESPONSE;
+
+    auto statusCode = etl::to_arithmetic<unsigned int>(field2);
+    if(!statusCode.has_value())
+      return Result::INVALID;
+
+    this->response.statusCode = statusCode.value();
+    // this->http.response.statusMessage = field3;
+  } else if(field3 == "HTTP/1.1")  // Request
+  {
+    this->messageType = Type::REQUEST;
+    this->request.method.assign(field1.begin(), field1.end());
+    this->request.endpoint.assign(field2.begin(), field2.end());
+
+    // Verify fields
+    if(this->request.method.empty() || this->request.endpoint.empty())
+      return Result::INVALID;
+
+    if(this->request.method.is_truncated() ||
+       this->request.endpoint.is_truncated())
+      return Result::INVALID;
+  } else {
+    return Result::INVALID;
+  }
+
+  // Advance iterator to header section
+  this->_it += end + sizeof(EOL_DELIMITER) - 1;
+
+  return Result::OK;
+}
+
+HTTPMessage::Result HTTPMessage::_parseHeader()
+{
+  etl::string_view buffer_view(
+      &(*this->_it), std::distance(this->_it, this->_buffer.end()));
 
   // Find header-body separator
-  size_t sep = buffer_view.find("\r\n\r\n");
+  size_t sep = buffer_view.find(BODY_DELIMITER);
+  if(sep == etl::string_view::npos)
+    return Result::INCOMPLETE;
 
   // Split by lines
   size_t pos = 0;
   while(pos < sep) {
-    size_t end = buffer_view.find("\r\n", pos);
-    if(end == std::string::npos)
-      return false;
+    size_t end = buffer_view.find(EOL_DELIMITER, pos);
+    if(end == etl::string_view::npos)
+      return Result::INVALID;
 
     etl::string_view line = buffer_view.substr(pos, end - pos);
     pos = end + 2;
@@ -57,61 +117,185 @@ bool HTTPMessage::parse(etl::string_view& buffer_view, HTTPMessage& message)
     if(line.empty())
       break;
 
-    if(message.messageType == Type::UNDEFINED) {
-      // Try to parse start line
-      etl::string_view field1, field2, field3;
-      size_t space1 = line.find(' ');
-      size_t space2 = line.find(' ', space1 + 1);
+    // Parse headers
+    size_t colon = line.find(':');
+    if(colon == etl::string_view::npos)
+      return Result::INVALID;
 
-      if(space1 == etl::istring::npos || space2 == etl::istring::npos)
-        return false;
+    // Insert key-value pair into the headers map
+    HeaderMap::key_type key(line.substr(0, colon).data());
+    HeaderMap::mapped_type value(line.substr(colon + 2).data());
 
-      field1 = line.substr(0, space1);
-      field2 = line.substr(space1 + 1, space2 - space1 - 1);
-      field3 = line.substr(space2 + 1);
+    this->headers.insert_or_assign(key, value);
+  }
 
-      if(field1.empty() || field2.empty() || field3.empty())
-        return false;
+  // Advance iterator to body section
+  this->_it += sep + sizeof(BODY_DELIMITER) - 1;
 
-      // Check if it's a request or response
-      if(field1 == "HTTP/1.1")  // Response
-      {
-        message.messageType = HTTPMessage::Type::RESPONSE;
+  this->_parserState = ParseState::BODY;
+  return Result::OK;
+}
 
-        auto statusCode = etl::to_arithmetic<unsigned int>(field2);
-        if(!statusCode.has_value())
-          return false;
+HTTPMessage::Result HTTPMessage::_parseBody()
+{
+  etl::string_view buffer_view(
+      &(*this->_it), std::distance(this->_it, this->_buffer.end()));
 
-        message.response.statusCode = statusCode.value();
-        // message.http.response.statusMessage = field3;
-      } else if(field3 == "HTTP/1.1")  // Request
-      {
-        message.messageType = HTTPMessage::Type::REQUEST;
-        message.request.method.assign(field1.begin(), field1.end());
-        message.request.endpoint.assign(field2.begin(), field2.end());
+  auto it = this->headers.find("Content-Length");
 
-        if(message.request.method.is_truncated() ||
-           message.request.endpoint.is_truncated())
-          return false;
-      } else {
-        return false;
-      }
-    } else {
-      // Parse headers
-      size_t colon = line.find(':');
-      if(colon == std::string::npos)
-        return false;
+  // Message does not contain explicit body length
+  if(it == this->headers.end())
+    return Result::INVALID;
 
-      if(!message.headers.full()) {
-        HeaderMap::key_type key(line.substr(0, colon));
-        HeaderMap::mapped_type value(line.substr(colon + 2));
+  size_t contentLength =
+      etl::to_arithmetic<size_t>(etl::string_view(it->second.data()));
 
-        message.headers.insert(etl::make_pair(key, value));
-      } else {
-        LOG_WARNING("HTTPMessage: too many headers");
-      }
+  if(std::distance(this->_it, this->_buffer.end()) < contentLength)
+    return Result::INCOMPLETE;
+
+  if(contentLength == 0) {
+    this->body.assign(nullptr, nullptr);
+  } else {
+    this->body.assign(&(*this->_it), &(*(this->_it + contentLength)));
+  }
+
+  return Result::OK;
+}
+
+// Parses HTTP message from provided data chunks
+// Returns OK if the message is complete and valid
+// If called after successful parsing, previous result will be invalidated
+// Calling this function with pending chunks will return INCOMPLETE
+HTTPMessage::Result HTTPMessage::parse(etl::string_view& buffer_view)
+{
+  // Enforce maximum message size limit
+  size_t newSize = this->_buffer.size() + buffer_view.size();
+  if(newSize > MAX_MESSAGE_LENGTH) {
+    this->_buffer.clear();
+    return Result::INVALID;
+    ;
+  }
+
+  // Parsing is done on a dynamic internal buffer
+  // to provide support for chunked messages
+  this->_buffer.insert(
+      this->_buffer.end(), buffer_view.begin(), buffer_view.end());
+
+  Result res = Result::INCOMPLETE;
+
+  // Parse message
+  while(res == Result::INCOMPLETE) {
+    switch(this->_parserState) {
+      case ParseState::START_LINE:
+        // Erase previous message
+        if(this->_it != this->_buffer.begin()) {
+          this->_buffer.erase(this->_buffer.begin(), this->_it);
+          this->_it = this->_buffer.begin();
+        }
+
+        res = this->_parseStartLine();
+
+        if(res == Result::OK)
+          this->_parserState = ParseState::HEADER;
+        break;
+
+      case ParseState::HEADER:
+        res = this->_parseHeader();
+
+        if(res == Result::OK)
+          this->_parserState = ParseState::BODY;
+        break;
+
+      case ParseState::BODY:
+        res = this->_parseBody();
+        break;
     }
   }
 
-  return true;
+  // Reset parser state if parsing has ended
+  if(res == Result::OK || res == Result::INVALID) {
+    this->_parserState = ParseState::START_LINE;
+  }
+
+  // Invalidate entire buffer on error
+  if(res == Result::INVALID) {
+    this->_buffer.clear();
+    this->_it = this->_buffer.begin();
+  }
+
+  return res;
+
+  // // Find header-body separator
+  // size_t sep = buffer_view.find("\r\n\r\n");
+
+  // // Split by lines
+  // size_t pos = 0;
+  // while(pos < sep) {
+  //   size_t end = buffer_view.find("\r\n", pos);
+  //   if(end == std::string::npos)
+  //     return false;
+
+  //   etl::string_view line = buffer_view.substr(pos, end - pos);
+  //   pos = end + 2;
+
+  //   if(line.empty())
+  //     break;
+
+  //   if(message.messageType == Type::UNDEFINED) {
+  //     // Try to parse start line
+  //     etl::string_view field1, field2, field3;
+  //     size_t space1 = line.find(' ');
+  //     size_t space2 = line.find(' ', space1 + 1);
+
+  //     if(space1 == etl::istring::npos || space2 == etl::istring::npos)
+  //       return false;
+
+  //     field1 = line.substr(0, space1);
+  //     field2 = line.substr(space1 + 1, space2 - space1 - 1);
+  //     field3 = line.substr(space2 + 1);
+
+  //     if(field1.empty() || field2.empty() || field3.empty())
+  //       return false;
+
+  //     // Check if it's a request or response
+  //     if(field1 == "HTTP/1.1")  // Response
+  //     {
+  //       message.messageType = HTTPMessage::Type::RESPONSE;
+
+  //       auto statusCode = etl::to_arithmetic<unsigned int>(field2);
+  //       if(!statusCode.has_value())
+  //         return false;
+
+  //       message.response.statusCode = statusCode.value();
+  //       // message.http.response.statusMessage = field3;
+  //     } else if(field3 == "HTTP/1.1")  // Request
+  //     {
+  //       message.messageType = HTTPMessage::Type::REQUEST;
+  //       message.request.method.assign(field1.begin(), field1.end());
+  //       message.request.endpoint.assign(field2.begin(), field2.end());
+
+  //       if(message.request.method.is_truncated() ||
+  //          message.request.endpoint.is_truncated())
+  //         return false;
+  //     } else {
+  //       return false;
+  //     }
+  //   } else {
+  //     // Parse headers
+  //     size_t colon = line.find(':');
+  //     if(colon == std::string::npos)
+  //       return false;
+
+  //     if(!message.headers.full()) {
+  //       HeaderMap::key_type key(line.substr(0, colon));
+  //       HeaderMap::mapped_type value(line.substr(colon + 2));
+
+  //       message.headers.insert(etl::make_pair(key, value));
+  //     } else {
+  //       LOG_WARNING("HTTPMessage: too many headers");
+  //     }
+  //   }
+  // }
+
+  // return true;
 }
--- core/husarnet/http.h
+++ core/husarnet/http.h
@@ -3,16 +3,47 @@
 // License: specified in project_root/LICENSE.txt
 #pragma once
 
-#include <etl/map.h>
+#include <map>
+#include <memory>
+#include <string>
+#include <vector>
+
 #include <etl/string.h>
+#include <etl/string_view.h>
+#include <etl/variant.h>
 #include <etl/vector.h>
 
 class HTTPMessage {
  public:
-  HTTPMessage(){};
+  enum class Result
+  {
+    OK = 0,
+    INCOMPLETE,
+    INVALID,
+  };
 
-  static bool parse(etl::string_view& buffer_view, HTTPMessage& message);
-  
+  enum class Type
+  {
+    UNDEFINED = 0,
+    REQUEST,
+    RESPONSE,
+  };
+
+  enum class ParseState
+  {
+    START_LINE = 0,
+    HEADER,
+    BODY,
+  };
+
+  HTTPMessage()
+  {
+    this->_buffer.reserve(MAX_MESSAGE_LENGTH / 4);
+    this->_it = this->_buffer.begin();
+  };
+
+  Result parse(etl::string_view& buffer_view);
+
   // TODO: overflow check?
   size_t encode(etl::ivector<char>& buffer);
 
@@ -36,23 +67,38 @@ class HTTPMessage {
     unsigned int statusCode = 0;
   } response;
 
-  struct request {
+  struct {
     etl::string<7> method;
-    etl::string<32> endpoint;
+    etl::string<64> endpoint;
   } request;
-
-  enum class Type
-  {
-    UNDEFINED = 0,
-    REQUEST,
-    RESPONSE,
-  };
 
   Type messageType = Type::UNDEFINED;
 
   // TODO: approach to this, maybe use static buf with stringviews
   // maybe stl::unordered_map with static alloc
   // anyway, having static allocator to use in stl containers would be nice
-  using HeaderMap = etl::map<etl::string<32>, etl::string<32>, 16>;
+  using HeaderMap = std::map<std::string, std::string>;
   HeaderMap headers;
+
+  // Contains message body
+  // In case of request, it should be assigned to the body of the request
+  // In case of response, it will contain a view into built-in buffer
+  etl::string_view body;
+
+ private:
+  // etl::variant<etl::ivector<char>, std::vector<char>> _buffer;
+  std::vector<char> _buffer;
+  std::vector<char>::iterator _it;
+
+  ParseState _parserState = ParseState::START_LINE;
+
+  const etl::string<2> EOL_DELIMITER = "\r\n";
+  const etl::string<4> BODY_DELIMITER = "\r\n\r\n";
+
+  // TODO: make platform-specific
+  const size_t MAX_MESSAGE_LENGTH = 8192;
+
+  Result _parseStartLine();
+  Result _parseHeader();
+  Result _parseBody();
 };
