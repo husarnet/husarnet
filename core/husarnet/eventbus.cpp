d59439b82547f8e0a7e347cf55729a5c4e203285
Milosz Lagan <milosz.lagan@husarion.com>
2024-10-10 09:20:32 +0200

    core: http requests from eventbus

--- core/husarnet/eventbus.cpp
+++ core/husarnet/eventbus.cpp
@@ -3,6 +3,7 @@
 // License: specified in project_root/LICENSE.txt
 #include "husarnet/eventbus.h"
 
+#include "husarnet/http.h"
 #include "husarnet/husarnet_manager.h"
 #include "husarnet/logging.h"
 
@@ -34,17 +35,19 @@ void EventBus::periodic()
     }
 
     this->_lastConnectionAttempt = currentTime;
-    // if (this->manager->getEbAddresses().size() == 0)
-    //   return;
 
-    // // TODO: handle multiple EB addresses
-    // InetAddress ebAddress = {
-    //   .ip = this->manager->getEbAddresses()[0],
-    //   .port = 80,
-    // };
+    // Connect to EB WS with Husarnet address from the license file
+    if(this->manager->getEbAddresses().size() == 0)
+      return;
 
-    // this->ws.connect(ebAddress, "/device/device_ws");
-    this->ws.connect(InetAddress::parse("127.0.0.1:8088"), "/");
+    // TODO: handle multiple EB addresses
+    InetAddress ebAddress = {
+        .ip = this->manager->getEbAddresses()[0],
+        .port = 80,
+    };
+
+    this->ws.connect(ebAddress, "/device/device_ws");
+    // this->ws.connect(InetAddress::parse("127.0.0.1:8088"), "/");
   }
 }
 
@@ -60,9 +63,79 @@ void EventBus::_handleMessage(WebSocket::Message& message)
   etl::string_view data(message.data.begin(), message.data.size());
 
   if(data.compare("getConfig") == 0) {
-    // Send config
-    this->ws.send("Hello from Husarnet daemon!");
+    this->_handleGetConfig();
   } else {
     LOG_WARNING("Unknown EB message: %s", data.data());
   }
+}
+
+void EventBus::_handleGetConfig()
+{
+  // Send config request to the EB
+  etl::vector<char, 256> buffer;
+
+  HTTPMessage http;
+  http.request.method = "GET";
+  http.request.endpoint = "/device/get_config";
+
+  http.encode(buffer);
+
+  // TODO: handle multiple EB addresses
+  assert(this->manager->getEbAddresses().size() > 0);
+  InetAddress ebAddress = {
+      .ip = this->manager->getEbAddresses()[0],
+      .port = 80,
+  };
+
+  int fd = OsSocket::connectUnmanagedTcpSocket(ebAddress);
+  if(fd == -1) {
+    LOG_ERROR("Failed to connect to EB");
+    return;
+  }
+
+  int res = send(fd, buffer.data(), buffer.size(), 0);
+
+  if(res == -1) {
+    LOG_ERROR("Failed to send message to EB");
+    close(fd);
+    return;
+  }
+
+  // Receive response until the connection is closed
+  // or a full HTTP message has been received
+  HTTPMessage::Result httpResult = HTTPMessage::Result::INVALID;
+
+  while(true) {
+    buffer.clear();
+    res = recv(fd, buffer.data(), buffer.capacity(), 0);
+
+    if(res == -1) {
+      LOG_ERROR("Failed to receive message from EB");
+      close(fd);
+      return;
+    }
+
+    if(res == 0) {
+      LOG_INFO("EB connection closed");
+      close(fd);
+      return;
+    }
+
+    etl::string_view response(buffer.data(), res);
+    httpResult = http.parse(response);
+
+    // Exit on first parsed or failed message
+    if(httpResult != HTTPMessage::Result::INCOMPLETE)
+      break;
+  }
+
+  if(httpResult == HTTPMessage::Result::INVALID) {
+    LOG_ERROR("Failed to receive message from EB");
+    close(fd);
+    return;
+  }
+
+  // Handle the received message
+  LOG_INFO("Received message from EB");
+  close(fd);
 }
