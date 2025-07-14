b3b0b32846ae154fea17dab2a03e227801c51cc1
ympek <24464784+ympek@users.noreply.github.com>
2025-07-14 16:59:55 +0000

    Build version 2.0.321

--- core/husarnet/husarnet_config.h
+++ core/husarnet/husarnet_config.h
@@ -10,7 +10,7 @@
 #include "sdkconfig.h"
 #endif
 
-#define HUSARNET_VERSION "2.0.320"
+#define HUSARNET_VERSION "2.0.321"
 #define HUSARNET_USER_AGENT "Husarnet," PORT_NAME "," PORT_ARCH "," HUSARNET_VERSION
 
 #define BASESERVER_PORT 443
